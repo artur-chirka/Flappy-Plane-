336c33621116ad0f9436dbc6a152be8f372cdadf
artegrr <foster2196@gmail.com>
2020-06-11 19:35:05 +0300

    updates

--- app/game.js
+++ app/game.js
@@ -2,28 +2,79 @@ const flappyPlane = document.getElementById("planeGame");
 const context = flappyPlane.getContext("2d");
 
 const plane = new Image();
-    plane.src = "img/planeRed1.png";
+plane.src = "img/planeRed1.png";
 
 const background = new Image();
-    background.src = "img/background.png";
+background.src = "img/background.png";
 
 const foreground = new Image();
-    foreground.src = "img/groundGrass.png";
+foreground.src = "img/groundGrass.png";
 
 const hillUp = new Image();
-    hillUp.src = "img/rockGrass.png";
+hillUp.src = "img/rockGrass.png";
 
 const hillDown = new Image();
-    hillDown.src = "img/rockGrassDown.png";
+hillDown.src = "img/rockGrassDown.png";
 
-let gap = 90; //? Отступ между холмами
+//? Отступ между холмами
+let gap = 200;
 
+//?Управление
+document.addEventListener('keydown', moveUp);
+
+function moveUp() {
+    positionY -= 25;
+}
+
+//Отрисовка холмов
+let hill = [];
+
+hill[0] = {
+    x: flappyPlane.width,
+    y: 0
+}
+
+
+
+//?Полет 
+let positionX = 350;
+let positionY = 150;
+let flight = 1;
+
+
+//*Рендер
 function render() {
     context.drawImage(background, 0, 0);
-    context.drawImage(hillDown, 100, 0);
-    context.drawImage(hillUp, 100, 0 + hillUp.height + gap);
+
+    for (let i = 0; i < hill.length; i++) {
+        context.drawImage(hillDown, hill[i].x, hill[i].y, 100, 250);
+        context.drawImage(hillUp, hill[i].x, hill[i].y + hillUp.height + gap, 100, 250);
+
+        hill[i].x--;
+
+        if (hill[i].x == 550) {
+            hill.push({
+                x: flappyPlane.width,
+                y: Math.floor(Math.random() * hillUp.height) - hillUp.height
+            });
+        }
+
+        //Отслеживание ударов
+        if (positionX + plane.width >= hill[i].x &&
+            positionX <= hill[i].x + hillUp.width &&
+            (positionY <= hill[i].y + hillUp.height ||
+                positionY + plane.height >= hill[i].y + hillUp.height + gap || positionY + plane.height >= flappyPlane.height - foreground.height)) {
+            location.reload(); // Перезагружаем игру
+        }
+
+
+    }
+
     context.drawImage(foreground, 0, flappyPlane.height - foreground.height);
-    context.drawImage(plane, 10, 150, 45, 35)
+
+    context.drawImage(plane, positionX, positionY, 45, 35)
+    positionY += flight;
+    requestAnimationFrame(render);
 
 }
 
